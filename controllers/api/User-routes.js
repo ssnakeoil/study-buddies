3653679c608ae03e6c41168a792f32b1406b1d48
yasmine <yasmineashoush5@gmail.com>
2023-01-29 23:09:52 -0500

    my front-end

--- controllers/api/User-routes.js
+++ controllers/api/User-routes.js
@@ -2,16 +2,16 @@ const router = require("express").Router();
 const { User } = require("../../models");
 
 
-router.post("/", async(req,res)=> {
-    try{
+router.post("/", async (req, res) => {
+    try {
         const userData = await User.create(req.body)
 
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
-      
-            res.status(200).json(userData);  
-          });
+
+            res.status(200).json(userData);
+        });
 
     } catch (err) {
         console.log(err);
